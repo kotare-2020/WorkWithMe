c3be786a985ee2b07b9df768c78911fa9f66b871
andrewtempany <andrewtempany95@gmail.com>
2020-06-12 10:45:06 +1200

    add shuffle function

--- db.js
+++ db.js
@@ -5,6 +5,20 @@ const connection = knex(config[env])
 //functions
 
 
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+function shuffle(arr) {
+    for (let initialPlace = arr.length - 1; initialPlace > 0; initialPlace--) {
+        const randomPlace = Math.floor(Math.random() * (initialPlace + 1));
+        [arr[initialPlace], arr[randomPlace]] = [arr[randomPlace], arr[initialPlace]]
+    }
+    return a
+}
+
+
+
+
+
 module.exports = {
 
 }
