b61643c28a47a0bcf7d7f55ecd9fc25adafec718
marta-machura <marta.machura@hotmail.com>
2020-06-12 10:34:31 +1200

    add seeds back into db

--- db.js
+++ db.js
@@ -2,12 +2,20 @@ const knex = require('knex')
 const config = require('./knexfile')
 const env = process.env.NODE_ENV || 'development'
 const connection = knex(config[env])
+
 //functions
 
-// function getPeople(db = connection){
-//   return db('people').select()
-// }
+ function getStudents(db = connection){
+   return db('students').select()
+ }
+
+function getStudentTopics (id, db = connection) {
+  return db('students')
+  .join('topics', 'topics.id', 'students.id'
+  //'students.id AS student_id')
+}
+
 
 module.exports = {
-
+  getStudents: getStudents
 }
